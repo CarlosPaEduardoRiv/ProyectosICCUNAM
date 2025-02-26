f00313ce5e5ad5b339eed0c0e747a8bf9cc89b7b
CarlosPaEduardoRiv <palacio0503eduardocr@gmail.com>
2025-02-25 23:06:08 -0600

    cambios de la practica

--- practica01/Main.java
+++ practica01/Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 public class Main{
   public static void main(String[] args){
 
-  Scanner in = new Scanner(System.in);
+  Scanner scanner1 = new Scanner(System.in);
+  Scanner scanner2 = new Scanner(System.in);
   String nombre = "";
-  int base = 0;
+  String puntoextra = "";
+  int num_base = 0;
   double num_uno = 0, num_dos = 0, resultado = 0;
 
    System.out.println("~~~~~~~~~~~~~~");
@@ -14,57 +16,64 @@ public class Main{
    System.out.print("");
   
    
-   System.out.println("Bienvenido a la calculadora malota: Ingrese su nombre, presione 1 para suma, 2 para multiplicacion y 3 para division; ");
+   System.out.println("Bienvenido a la calculadora malota: Ingrese su nombre, presione 1 para suma, 2 para multiplicacion, 3 para division y 4 para escribir texto a eleccion ");
  
    System.out.println("Introduce tu nombre: ");
-   nombre = in.nextLine();
+   nombre = scanner1.nextLine();
 
-   System.out.println("Ingrese un numero entre el 1 y el 3");
+   System.out.println("Ingrese un numero previamente mencionado");
 
-   base = in.nextInt(); 
+   num_base = scanner1.nextInt(); 
 
-   if(base == 1){
+   if(num_base == 1){
    
     System.out.println("Ingresa el primer numero: ");
-    num_uno = in.nextInt();
+    num_uno = scanner1.nextInt();
 
     System.out.println("Ingresa el segundo numero: ");
-    num_dos = in.nextInt();
+    num_dos = scanner1.nextInt();
 
     resultado = num_uno + num_dos;
 
     System.out.print("");
 
     System.out.println(nombre + ", el resultado de la suma es: " + resultado);
-  } else if(base == 2){
+  } else if(num_base == 2){
     
     System.out.println("Ingresa el primer numero: ");
-    num_uno = in.nextInt();
+    num_uno = scanner1.nextInt();
 
     System.out.println("Ingresa el segundo numero: ");
-    num_dos = in.nextInt();
+    num_dos = scanner1.nextInt();
 
     resultado = num_uno * num_dos;
 
     System.out.print("");
 
     System.out.println(nombre + ", el resultado de la multiplicacion es: " + resultado);
-  } else if(base == 3){
+  } else if(num_base == 3){
  
     System.out.println("Ingresa el primer numero: ");
-    num_uno = in.nextInt();
+    num_uno = scanner1.nextInt();
 
     System.out.println("Ingresa el segundo numero: ");
-    num_dos = in.nextInt();
+    num_dos = scanner1.nextInt();
 
     resultado = num_uno / num_dos;
 
     System.out.print("");
 
     System.out.println(nombre + ", el resultado de la division es: " + resultado);
+  } else if (num_base == 4){
+
+    System.out.println("Ingrese su texto a eleccion");
+    puntoextra = scanner2.nextLine();
+    System.out.println(nombre + ", su texto es: " + puntoextra);
+    System.out.print("");
+    
   } else{
 
-    System.out.println("Numero invalido " + nombre+ " , elige un numero entre {1,2,3}");
+    System.out.println("Numero invalido " + nombre+ " , elige un numero entre 1,2,3 o 4");
   }
  } 
 }
