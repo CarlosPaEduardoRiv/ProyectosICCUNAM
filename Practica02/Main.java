8039f552348ca2d65bf7c4eb6c000c548c3ea338
CarlosPaEduardoRiv <palacio0503eduardocr@gmail.com>
2025-03-23 19:50:11 -0600

    practica 2 actualizada

--- Practica02/Main.java
+++ Practica02/Main.java
@@ -1,68 +1,48 @@
-public class Main{
+public class Main {
 
-  private int segundos;
   private int minutos;
   private int horas;
-  
 
-  public Main(int segundos, int minutos, int horas){
-
-  this.minutos = minutos;
-  this.horas = horas;
-  this.segundos = segundos;
-
+  public Main(int minutos, int horas) {
+      this.minutos = minutos;
+      this.horas = horas;
   }
-  public void manecillaSegundo(){
-    segundos++;
-    if(segundos == 60){
-      segundos = 0;
-      manecillaMinuto();
-    }
-  }
-  public void manecillaMinuto(){
+
+  public void manecillaMinuto() {
+       minutos++;
+      if (minutos == 60) {
+          minutos = 0;
+          manecillaHora();
+      }
     
-    for(int i = 0; i < 61 ; i++ ){
-     minutos++;
-     manecillaHora();
-     if(minutos == 60){
-      minutos = 0;
-     } 
-    }
   }
-  
-  public void manecillaHora(){
-    
-    do{
+
+  public void manecillaHora() {
       horas++;
-      if(horas == 24){
-        horas = 0;
+      if (horas == 24) {
+          horas = 0;
       }
-    }while(minutos == 60);
   }
-  
-  public void Manecillas(){
 
-    System.out.printf("%02d:%02d:%02d%n", horas, minutos, segundos);
-  } 
+  public void Manecillas() {
+      System.out.printf("%02d:%02d%n", horas, minutos);
+  }
 
-  public void mostrarHora(){
-    while(true){
-      Manecillas();
-      manecillaSegundo();
-      manecillaHora();
-      
-      try {
-        Thread.sleep(1000);
-      } catch (InterruptedException e){
-        e.printStackTrace();
+  public void mostrarHora() {
+      while (true) {
+          Manecillas();
+          manecillaMinuto();
+
+          try {
+              Thread.sleep(60000); 
+          } catch (InterruptedException e) {
+              e.printStackTrace();
+          }
       }
-    }
   }
+
   public static void main(String[] args) {
-    Main reloj = new Main(50, 49, 13);
-    reloj.mostrarHora();
-    
+      Main reloj = new Main(25, 19);
+      reloj.mostrarHora();
   }
-
 }
-
